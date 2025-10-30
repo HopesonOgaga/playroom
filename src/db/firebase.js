84a40c16e85a5d27be857af887f9a53c1eb9a47f
Ogaga Hopeson Tega <hopesonogaga@gmail.com>
2025-10-30 01:03:13 +0100

    security breach

--- src/db/firebase.js
+++ src/db/firebase.js
@@ -5,12 +5,12 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCBTGHXXsekuTTlNm7pnnbCwkrDLK2VDkQ",
+  apiKey: FIRE_BASE_TOKEN,
   authDomain: "playroom-a8d5a.firebaseapp.com",
   projectId: "playroom-a8d5a",
   storageBucket: "playroom-a8d5a.appspot.com", // ✅ fixed here
-  messagingSenderId: "237198485447",
-  appId: "1:237198485447:web:75e0761720d3dfec26dbe2",
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
   measurementId: "G-9T7B4RZR5R"
 };
 
